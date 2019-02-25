055bd93fb598eeb665c0e2eefbae0866f15a8a1a
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-25 11:17:12 -0500

    React-slick implemented

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,7 @@ import Home from './components/home/Home';
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container center">
         <Home />
       </div>
     );
--- front-end/src/components/home/Carousel.js
+++ front-end/src/components/home/Carousel.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Carousel(props){
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
+
     return(
-        <h1>PRESS DOWN TO DUCK!</h1>
+        <Slider>
+            <div>
+                <h3>PRESS UP TO LOOK UP!</h3>
+            </div>
+            <div>
+                <h3>PRESS LEFT TO MOVE LEFT!</h3>
+            </div>
+            <div>
+                <h3>PRESS RIGHT TO MOVE RIGHT!</h3>
+            </div>
+            <div>
+                <h3>PRESS DOWN TO DUCK!</h3>
+            </div>
+        </Slider>
     )
 }
 

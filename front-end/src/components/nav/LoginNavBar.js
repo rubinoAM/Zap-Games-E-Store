0abbc2328da942cb741aac005025a4e4bfe19392
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-25 14:27:50 -0500

    Beginning log-in via Github

--- front-end/src/components/nav/LoginNavBar.js
+++ front-end/src/components/nav/LoginNavBar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 function LoginNavBar(props){
+    /*
+        Process:
+        1) User clicks and opens new window via loginTab
+        2) New window is open to crossOrigin but is github.com
+        3) Once user authenticates, github sends them to /auth/github/callback
+        4) The callback URL either gets the uid or inserts them
+        5) Callback takes uid and tokenizes it with JWT
+        6) Token is sent back to github window that loginTab opened and
+           window.opener.postMessage is in the script of that window which
+           sends the data back over to the original page
+        7) It's now available in this promise resolution
+        8) Put it in localstorage so we can use it next time
+    */
+
     return(
         <div className="login-nav-bar">
             <div className="left valign-wrapper brand-logo">ZAP GAMES</div>
-            <div className="right">MY CART 0 ITEM - £0.00</div>
+            <div className="right">
+                <button type="submit" class="btn play-button btn-github">Login with github</button>
+                <span>MY CART 0 ITEM - $0.00</span>
+            </div>
         </div>
     )
 }
--- front-end/src/components/nav/MainNav.js
+++ front-end/src/components/nav/MainNav.js
@@ -8,7 +8,7 @@ function MainNav(props){
                     <ul className="left hide-on-med-and-down">
                         <li><a href="sass.html">Sass</a></li>
                         <li><a href="badges.html">Components</a></li>
-                        <li className="active"><a href="collapsible.html">JavaScript</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
                     </ul>
                 </div>
         </nav>      

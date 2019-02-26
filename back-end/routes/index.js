f6a3e4325a238e01cd6d4ba3f7935692d9a05c24
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-26 14:27:54 -0500

    Beginning to implement pg-promise

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router();
 const config = require('../config');
 const passport = require('passport');
+const pgp = require('pg-promise')();
+const connection = config.pgp;
+const db = pgp(connection);
 
 //Github Auth Routes
 router.get('/auth/github', passport.authenticate('github'));
 
 router.get('/auth/github/callback', passport.authenticate('github'), (req,res,next)=>{
-  res.json(req.user);
+  const selectQuery = `SELECT * FROM users`;
+  const pgPromise = db.query(selectQuery);
+  //console.log(pgPromise);
+  pgPromise.then((data)=>{
+    res.json(data);
+  })
+  //res.json(req.user);
 });
 
 module.exports = router;

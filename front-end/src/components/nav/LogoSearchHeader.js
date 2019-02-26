4661ba1643dc497bde80a3792f0b9491a366bf75
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-26 09:18:35 -0500

    Fixed search bar

--- front-end/src/components/nav/LogoSearchHeader.js
+++ front-end/src/components/nav/LogoSearchHeader.js
@@ -3,12 +3,11 @@ import React, { Component } from 'react';
 function LogoSearchHeader(props){
     return(
         <div className="logo-search-header">
-            <div className="left-align col search-icon">
-                <i className="small material-icons">search</i>
-            </div>
-            <div className="right-align col search-input">
-                <input type="text" placeholder="Search" />
-            </div>            
+            <div class="input-field col s12">
+                <i class="material-icons prefix">search</i>
+                <input id="icon_prefix" type="text" class="validate"/>
+                <label for="icon_prefix">Search</label>
+            </div>         
         </div>
     )
 }

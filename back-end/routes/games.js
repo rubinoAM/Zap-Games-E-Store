44320e527a965deaeaac8bbbd20a3e01a8942199
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-28 14:18:26 -0500

    /games/getHome CONNECTED

--- back-end/routes/games.js
+++ back-end/routes/games.js
@@ -5,7 +5,14 @@ const db = require('../database');
 //This middleware is only applied at /games
 
 router.get('/getHome',(req,res,next)=>{
-    res.json("GAMES");
+    const gameQuery = `SELECT * FROM games
+        WHERE screenshot_url IS NOT NULL
+        ORDER BY popularity DESC
+        LIMIT 4;`;
+
+        db.query(gameQuery).then((results)=>{
+            res.json(results);
+        }).catch((err)=>{if(err){throw err}});
 });
 
 module.exports = router;

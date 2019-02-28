292da1063a137a7bdcd8da2e121c491644aa6d0e
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-28 14:26:29 -0500

    Revamping GameCard layout

--- front-end/src/components/utilities/GameCard.js
+++ front-end/src/components/utilities/GameCard.js
@@ -5,18 +5,18 @@ function GameCard(props){
     let ran = Math.floor(Math.random() * images.length);
 
     return(
-        <div className="col s6 l3 game-card">
+        <div className="col s12 m6 l3 game-card">
             <div className="card">
                 <div className="card-image waves-effect waves-block waves-light">
                 <img alt="" className="activator" src={images[ran]} />
                 </div>
                 <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">{props.data.name}<i className="material-icons right">more_vert</i></span>
-                <p><a href="#">This is a link</a></p>
+                <span className="card-title activator grey-text text-darken-4">{props.data.name}</span>
+                <p><a href="#">Purchase</a></p>
                 </div>
                 <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                <span className="card-title grey-text text-darken-4">{props.data.name}<i className="material-icons right">close</i></span>
+                <p>{props.data.summary}</p>
                 </div>
             </div> 
         </div>

4560fdb05de2bcce819966109f9d4524433faa0e
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-28 10:31:27 -0500

    Basis single game page finished

--- front-end/src/components/pages/Game.js
+++ front-end/src/components/pages/Game.js
@@ -14,16 +14,30 @@ class Game extends Component{
         return(
             <div className="game-container">
                 <div className="row">
-                    <div className="col s12">
-                        <h1>GAME TITLE</h1>
-                    </div>
-                </div>
-                <div className="row">
                     <div className="col s12 m4">
-                        <img src="http://via.placeholder.com/300" alt="" className="game-pic" />
+                        <img src="http://via.placeholder.com/250" alt="" className="game-pic" />
                     </div>
                     <div className="col s12 m8">
-                        <p>D</p>
+                        <div className="row game-main-details">
+                            <h3 className="game-title">GAME TITLE</h3>
+                            <div className="game-desc">
+                                <p>Quisque tellus ante, ornare eget neque eget, pulvinar congue eros. Quisque id mollis libero. Aliquam luctus id justo in rhoncus. Suspendisse pharetra, leo vel porta ultrices, tortor elit molestie ex, ut convallis nibh elit ut elit. Praesent egestas elit vitae placerat faucibus. Mauris feugiat</p>
+                                <p>Maecenas tristique metus nec viverra sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam libero lacus, venenatis nec urna eget, viverra sollicitudin sem. Vestibulum eu enim vitae erat finibus ultrices vitae ac purus</p>
+                                <p>Duis hendrerit semper nulla ac ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae sagittis mi, non mattis eros. Praesent bibendum feugiat erat, nec iaculis dolor pharetra at. Phasellus leo ligula, venenatis ac massa nec, tempor scelerisque mauris</p>
+                                <p>Fusce mauris mi, placerat in elit id, imperdiet consequat erat. Sed vitae turpis id tellus eleifend laoreet id eu tellus. Quisque volutpat molestie orci id mattis. Maecenas ut tempus quam. Nulla facilisi. Aliquam erat volutpat. Donec faucibus dolor dui, a placerat velit convallis ac. Donec luctus ex mi, sed rhoncus orci cursus ultricies. Ut ut lacinia libero. Maecenas pulvinar enim nec lectus tempor venenatis. Phasellus at enim felis.</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col s1">
+                                <span>Qty:</span>
+                            </div>
+                            <div className="col s8">
+                                <input type="text" name="quantity"/>
+                            </div>
+                            <div className="col s2">
+                                <button className="btn amber lighten-2 black-text waves-effect waves-dark">ADD</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ front-end/src/misc/tab.js
@@ -0,0 +1,1 @@
+var instance = M.Tabs.getInstance(elem);
